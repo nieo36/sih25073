6fe69d6b19936942d3da771b465da9d32880ae16
user.email <nischalswami3636@gmail.com>
2026-08-20 23:18:08 +0530

    fix

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,16 +1,17 @@
 const rateLimit = require('express-rate-limit');
 
-const rateLimiter = (maxRequest = 100, time = 15 * 60 * 1000) => {
+const rateLimiter = (maxRequest = 5000, time = 15 * 60 * 1000) => {
 	return rateLimit({
 		max: maxRequest,
 		windowMs: time,
-		message: 'Too many requests, please try again later',
+		message: { message: 'Too many requests, please try again later' },
 		headers: true,
 		standardHeaders: true,
 		legacyHeaders: false,
+		skip: () => process.env.NODE_ENV !== 'production', // Skip in development
 	});
 };
 
 module.exports = {
-	rateLimiter
+	rateLimiter,
 };
