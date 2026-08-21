0e3d20679f0b9f4a7470257054d5dc72c10de04b
user.email <nischalswami3636@gmail.com>
2026-08-21 19:47:35 +0530

    URI fix

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ const http = require('http');
 const dns = require('dns');
 try {
     dns.setServers(['8.8.8.8', '8.8.4.4']);
-} catch (e) {}
+} catch (e) { }
 require('dotenv').config();
 const { mongoConnect } = require('./src/config/db.js');
 const app = require('./app.js');
 
 const port = Number(process.env.PORT) || 2000;
-const mongo = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/geotager";
+const mongo = process.env.MONGO_URI;
 const host = process.env.HOST ?? "0.0.0.0";
 
 async function start() {
